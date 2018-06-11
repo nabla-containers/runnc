abd7307aa9c2969693f9a4203b271d7ed9339b59
Brandon Lum <lumjjb@gmail.com>
2018-06-11 14:47:46 -0400

    Updated COPYRIGHT

--- runnc.go
+++ runnc.go
@@ -1,5 +1,5 @@
 // Copyright (c) 2018, IBM
-// Author(s): Brandon Lum, Ricardo Koller
+// Author(s): Brandon Lum, Ricardo Koller, Dan Williams
 //
 // Permission to use, copy, modify, and/or distribute this software for
 // any purpose with or without fee is hereby granted, provided that the
