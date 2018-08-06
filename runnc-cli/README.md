823487a836ef346d616c6c9b42c698d0d1193bcb
Brandon Lum <lumjjb@gmail.com>
2018-08-06 15:31:04 -0400

    Created initial skeleton of runnc-cli
    Signed-off-by: Brandon Lum <lumjjb@gmail.com>

---
+++ runnc-cli/README.md
@@ -0,0 +1,3 @@
+This folder is to contain runnc cli implementation which will not use runc.
+
+The structure of the code is based around that of the original [`runc`](https://github.com/opencontainers/runc).
---
+++ runnc-cli/util.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"os"
+)
+
+// fatal prints the error's details if it is a libcontainer specific error type
+// then exits the program with an exit status of 1.
+func fatal(err error) {
+	// make sure the error is written to the logger
+	logrus.Error(err)
+	fmt.Fprintln(os.Stderr, err)
+	os.Exit(1)
+}
