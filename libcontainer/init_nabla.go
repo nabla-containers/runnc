9e7ff9602659d5e74360a0f86ac79fadbd67a12e
Brandon Lum <lumjjb@gmail.com>
2018-09-10 13:41:25 -0400

    Added copyright and gofmt
    Signed-off-by: Brandon Lum <lumjjb@gmail.com>

--- libcontainer/init_nabla.go
+++ libcontainer/init_nabla.go
@@ -1,3 +1,17 @@
+// Copyright 2014 Docker, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package libcontainer
 
 import (
@@ -51,7 +65,7 @@ func initNabla() error {
 	if _, err := syscall.Write(fd, []byte("0")); err != nil {
 		return newSystemErrorWithCause(err, "write 0 exec fifo")
 	}
-    syscall.Close(fd)
+	syscall.Close(fd)
 	syscall.Close(rootfd)
 
 	if err := syscall.Exec(config.Args[0], config.Args, os.Environ()); err != nil {
